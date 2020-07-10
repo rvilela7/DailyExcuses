b6152b721c70b324ee3d056dcb03779be5e61e93
RuiVilela <ruivil@gmail.com>
2020-07-10 16:55:14 +0100

    Cenas

--- Controllers/RandomExcuse.cs
+++ Controllers/RandomExcuse.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
@@ -21,10 +22,26 @@ namespace DailyExcuses.Controllers
         public string Get()
         {
             string[] excuses = System.IO.File.ReadAllLines(excusesFile);
-
             Random rnd = new Random();
             int i = rnd.Next(excuses.Count());
             return excuses[i];
+        }
+
+        [HttpPost("/New")]
+        public IActionResult InsertNew([FromBody] string excuse)
+        {   
+            if (string.IsNullOrWhiteSpace(excuse))
+                return BadRequest("Requires valid excuse!");
+
+            System.IO.File.AppendAllText(excusesFile, excuse.Trim() + Environment.NewLine);
+            return Ok(excuse);
+        }
+
+        [HttpGet("/List")]
+        public List<string> GetList()
+        {
+            string[] excuses = System.IO.File.ReadAllLines(excusesFile);
+            return excuses.ToList();
         }
     }
 }
