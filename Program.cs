de0916fdb11e50cbce11ed5090eddf7c5030d6ee
Rui Vilela <ruivil@gmail.com>
2022-04-29 14:28:53 +0100

    Swagger prod fix

--- Program.cs
+++ Program.cs
@@ -9,11 +9,15 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (
+    app.Environment.IsDevelopment()
+    || app.Environment.IsProduction()
+    )
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
 // Added
 app.UseSwaggerUI(options =>
 {
