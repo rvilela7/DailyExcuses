5c0b182690a834ce5b96f9d52a7634ad2fbbfd1c
Rui Vilela <ruivil@gmail.com>
2022-11-08 16:42:06 +0000

    simplify namespace

--- Controllers/RandomExcuse.cs
+++ Controllers/RandomExcuse.cs
@@ -1,61 +1,61 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-namespace DailyExcuses.Controllers
+namespace DailyExcuses.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+[AllowAnonymous]
+public class RandomExcuse : ControllerBase
 {
-    [ApiController]
-    [Route("[controller]")]
-    [AllowAnonymous]
-    public class RandomExcuse : ControllerBase
+    private const string excusesFile = "excuses.txt";
+    private readonly ILogger<RandomExcuse> _logger;
+
+    public RandomExcuse(ILogger<RandomExcuse> logger)
     {
-        private const string excusesFile = "excuses.txt";
-        private readonly ILogger<RandomExcuse> _logger;
-
-        public RandomExcuse(ILogger<RandomExcuse> logger)
-        {
-            _logger = logger;
-        }
-
-        [HttpGet("/quote")]
-        [HttpGet("/excuses")]
-        public string Get()
-        {
-            string[] excuses = System.IO.File.ReadAllLines(excusesFile);
-            Random rnd = new Random();
-            int i = rnd.Next(excuses.Count());
-            return excuses[i];
-        }
-
-        [HttpPost("/new")]
-        [Obsolete("This method is obsolete.")]
-        public IActionResult InsertNew([FromBody] string excuse)
-        {   
-            if (!Debugger.IsAttached)
-            {
-                throw new NotImplementedException("Unavailable in production");
-            }
-
-            if (string.IsNullOrWhiteSpace(excuse))
-                return BadRequest("Requires valid excuse!");
-
-            System.IO.File.AppendAllText(excusesFile, excuse.Trim() + Environment.NewLine);
-            return Ok(excuse);
-        }
-
-        [HttpGet("/list")]
-        [Produces("application/json")]
-        public IActionResult GetList()
-        {
-            string[] excuses = System.IO.File.ReadAllLines(excusesFile);
-            return Ok(new JsonResult(excuses));
-        }
-
-        // Needs CORS and OPTIONS set in startup
-
-        // [HttpOptions]
-        // public IActionResult GetOptions()
-        // {
-        //     return Ok();
-        // }
+        _logger = logger;
     }
+
+    [HttpGet("/quote")]
+    [HttpGet("/excuses")]
+    public string Get()
+    {
+        string[] excuses = System.IO.File.ReadAllLines(excusesFile);
+        Random rnd = new Random();
+        int i = rnd.Next(excuses.Count());
+        return excuses[i];
+    }
+
+    [HttpPost("/new")]
+    [Obsolete("This method is obsolete.")]
+    public IActionResult InsertNew([FromBody] string excuse)
+    {
+        if (!Debugger.IsAttached)
+        {
+            throw new NotImplementedException("Unavailable in production");
+        }
+
+        if (string.IsNullOrWhiteSpace(excuse))
+            return BadRequest("Requires valid excuse!");
+
+        System.IO.File.AppendAllText(excusesFile, excuse.Trim() + Environment.NewLine);
+        return Ok(excuse);
+    }
+
+    [HttpGet("/list")]
+    [Produces("application/json")]
+    public IActionResult GetList()
+    {
+        string[] excuses = System.IO.File.ReadAllLines(excusesFile);
+        return Ok(new JsonResult(excuses));
+    }
+
+    // Needs CORS and OPTIONS set in startup
+
+    // [HttpOptions]
+    // public IActionResult GetOptions()
+    // {
+    //     return Ok();
+    // }
 }
+
